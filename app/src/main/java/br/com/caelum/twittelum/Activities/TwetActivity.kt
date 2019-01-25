78fd6a08a3d6992943726321daed1371a35a0dbc
Rafael Matsuda de Souza <rafamatsuda@gmail.com>
2019-01-25 20:53:01 -0200

    Pegando retorno da camera

--- app/src/main/java/br/com/caelum/twittelum/Activities/TwetActivity.kt
+++ app/src/main/java/br/com/caelum/twittelum/Activities/TwetActivity.kt
@@ -4,13 +4,18 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v4.content.FileProvider
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import br.com.caelum.twittelum.R
 import br.com.caelum.twittelum.bancoDeDados.TweetDao
@@ -19,8 +24,13 @@ import br.com.caelum.twittelum.modelo.Tweet
 import br.com.caelum.twittelum.viewmodel.TweetViewModel
 import br.com.caelum.twittelum.viewmodel.ViewModelFactory
 import kotlinx.android.synthetic.main.activity_lista_tweets.*
+import kotlinx.android.synthetic.main.activity_twet.*
+import java.io.File
+import java.net.URI
 
 class TwetActivity : AppCompatActivity() {
+
+    private var localFoto: String? = null
 
     private val viewModel : TweetViewModel by lazy {
         ViewModelProviders.of(this, ViewModelFactory).get(TweetViewModel::class.java)
@@ -38,6 +48,26 @@ class TwetActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.tweet_menu, menu)
 
         return true
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        localFoto?.let{
+            carregaFoto()
+        }
+    }
+
+    private fun carregaFoto() {
+
+        val bitmap = BitmapFactory.decodeFile(localFoto)
+
+        val bm = Bitmap.createScaledBitmap(bitmap, bitmap.width, bitmap.height, true)
+
+        FotoTweetForm.setImageBitmap(bm)
+
+        FotoTweetForm.scaleType = ImageView.ScaleType.FIT_XY
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -73,9 +103,23 @@ class TwetActivity : AppCompatActivity() {
     private fun tiraFoto(): Boolean {
 
         val vaiPraCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        val caminhoFoto = defineLocalDaFoto()
+
+        vaiPraCamera.putExtra(MediaStore.EXTRA_OUTPUT, caminhoFoto)
+
         startActivity(vaiPraCamera)
 
         return true
+    }
+
+    fun defineLocalDaFoto(): Uri? {
+
+        localFoto = "${getExternalFilesDir("fotos")}/${System.currentTimeMillis()}.jpg"
+
+        val arquivo = File(localFoto)
+
+        return FileProvider.getUriForFile(this, "MeuProvider", arquivo)
     }
 
     private fun publicaTweet(){
